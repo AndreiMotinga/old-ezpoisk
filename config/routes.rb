5c3518e713ad65bab351c305ba250623f716c114
Andrei Motinga <andrew.motinga@gmail.com>
2015-12-18 14:01:17 -0500

    remove real_estates index page

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,15 @@ Rails.application.routes.draw do
   resource :dashboard, only: [:show]
   resources :news
 
-  resource :real_estate, only: [:show]
   namespace :real_estate do
     resources :re_agencies, only: [:index, :show]
     resources :re_privates, only: [:index, :show]
+  end
+
+  namespace :dashboard do
+    namespace :real_estate do
+      resources :re_agencies, only: [:new]
+    end
   end
 
   root to: "home#index"
---
+++ spec/features/user_create_re_agency_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+feature "user create re_agency" do
+  scenario "successfully" do
+    # visit
+  end
+end
