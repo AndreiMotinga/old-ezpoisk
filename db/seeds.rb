d04516499cb096eb0bb8b3fdaa7e6d7d71f05505
Andrei Motinga <andrew.motinga@gmail.com>
2016-01-11 17:07:41 -0500

    seed file update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,14 @@
 require 'csv'
 
-User.create(email: 'test@test.com',
-            password: 'password',
-            password_confirmation: 'password',
-            name: 'Andrei',
-            phone: '9898989898',
-            admin: true
-           )
-
-# create states
 STATES.each { |s| State.create(name: s) }
 
 # create cities
-csv_text = File.read('public/us_cities_and_states.csv')
-csv = CSV.parse(csv_text, :headers => false)
+csv_text = File.read("public/us_cities_and_states.csv")
+csv = CSV.parse(csv_text, headers: false)
 csv.each do |row|
-  line = row[0].split('|')
+  line = row[0].split("|")
   state_name = line[1].strip
   state = State.find_by_name(state_name)
   city_name = line[2].strip
   City.create(name: city_name, state: state)
 end
-
