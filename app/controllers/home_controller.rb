dbce57f90399bd0fa5e3ab3ee175075a496ed7cd
Andrei Motinga <andrew.motinga@gmail.com>
2016-01-16 11:28:57 -0500

    * improve services layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @posts = Post.order("created_at desc").limit(10)
+    @posts = Post.order("created_at desc").limit(30)
     @main = Post.main
   end
 end
