bcd6809566ec15b6b31c8bd70cacc10ef859768d
Andrei Motinga <andrew.motinga@gmail.com>
2016-01-23 17:12:38 -0500

    improves dashboard re_agency styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  layout :layout_by_resource
 
   def forem_user
     current_user
   end
   helper_method :forem_user
-
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -19,6 +19,10 @@ class ApplicationController < ActionController::Base
   end
 
   protected
+
+  def layout_by_resource
+    "dashboard" if devise_controller?
+  end
 
   def address_changed?(record, prms)
     return true if record.try(:street) != prms[:street]
