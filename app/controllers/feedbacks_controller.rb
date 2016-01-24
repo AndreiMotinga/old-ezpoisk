eda866a8af55104147372be8e9b98096264b84c1
Andrei Motinga <andrew.motinga@gmail.com>
2016-01-24 18:10:25 -0500

    notify slack of new message

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -9,6 +9,7 @@ class FeedbacksController < ApplicationController
     feedback = Feedback.new(message_params)
     feedback.user = current_user
     if feedback.save
+      Ez.new.ping("new feedback")
       AdminMailerJob.perform_async(feedback.id, "Feedback")
       redirect_to root_path, notice: I18n.t(:thanks_for_feedback)
     else
