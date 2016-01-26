256f19eee83bc8391a5a8a2b5e639434e5ca9447
Andrei Motinga <andrew.motinga@gmail.com>
2016-01-26 11:08:29 -0500

    add navabar to services

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -13,17 +13,4 @@ describe Job do
   it { should belong_to(:user) }
   it { should belong_to(:city) }
   it { should belong_to(:state) }
-
-  describe "#address" do
-    it "return proper formatted address" do
-      state = create :state, name: "New York"
-      city = create :city, name: "Brooklyn", state: state
-      job = create :job,
-                   city: city,
-                   state: state,
-                   zip: 112_29
-
-      expect(job.address).to eq "Brooklyn New York, 11229"
-    end
-  end
 end
