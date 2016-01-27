6b696585b7b1827ffb53a1bfc5ca922942451b5a
Andrei Motinga <andrew.motinga@gmail.com>
2016-01-26 23:14:26 -0500

    show subcategory on services if category == 'Другие услуги'

--- app/controllers/dashboard/sales_controller.rb
+++ app/controllers/dashboard/sales_controller.rb
@@ -23,6 +23,7 @@ class Dashboard::SalesController < ApplicationController
     @sale = current_user.sales.build(sale_params)
 
     if @sale.save
+      AdminMailerJob.perform_async(@service.id, "Service")
       GeocodeJob.perform_async(@sale.id, "Sale")
       redirect_to edit_dashboard_sale_path(@sale),
                   notice: I18n.t(:post_saved)
