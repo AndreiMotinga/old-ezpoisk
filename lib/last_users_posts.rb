10bd53fefa2cdc7803d3d53e19115936667f4dd6
Andrei Motinga <andrew.motinga@gmail.com>
2016-02-25 11:41:20 -0500

    removes targets

--- lib/last_users_posts.rb
+++ lib/last_users_posts.rb
@@ -1,7 +1,7 @@
 class LastUsersPosts
   def last_posts(num)
     posts = RePrivate.last(100)
-    [Service, Job, Sale].each do |model|
+    [ReAgency, ReCommercial, ReFinance, Service, JobAgency, Job, Sale].each do |model|
       posts += model.last(100)
     end
     posts.sort_by(&:created_at).last(num).reverse
