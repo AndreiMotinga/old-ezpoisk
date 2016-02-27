0937276a98a244f5a5aa16566e9418d7d3ed5bcf
Andrei Motinga <andrew.motinga@gmail.com>
2016-02-27 08:58:08 -0500

    asdf

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,7 +4,7 @@ class Job < ActiveRecord::Base
   include Filterable
   include ViewHelpers
 
-  validates :title, presence: true, length: { maximum: 90, minimum: 5 }
+  validates :title, presence: true, length: { maximum: 90, minimum: 3 }
   validates :phone, presence: true
   validates :category, presence: true
   validates :post_type, presence: true
