b5a5ccd855f61610ea2a97af5f80513656e9cb50
Andrei Motinga <andrew.motinga@gmail.com>
2016-02-29 23:55:36 -0500

    prettify tags

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,27 +1,28 @@
 module QuestionsHelper
-  def upvote_btn(q)
-    link_to "Upvote | #{q.score}",
-      upvote_question_path(q),
-      method: :put,
-      remote: true,
-      class: "btn btn-success",
-      id: "upvote-#{q.id}"
+  def upvote_btn(question)
+    link_to("Upvote | #{question.score}",
+            upvote_question_path(question),
+            method: :put,
+            remote: true,
+            class: "btn btn-success",
+            id: "upvote-#{question.id}")
   end
 
-  def voted_btn(q)
-    link_to "Voted | #{q.score}",
-      unvote_question_path(q),
-      method: :put,
-      remote: true,
-      class: "btn btn-default",
-      id: "voted-#{q.id}"
+  def voted_btn(question)
+    link_to("Voted | #{question.score}",
+            unvote_question_path(question),
+            method: :put,
+            remote: true,
+            class: "btn btn-default",
+            id: "voted-#{question.id}")
   end
 
   def tag_links(question)
-    sanitize question.tag_list.map { |t| link_to t, tag_path(t) }.join(', ')
+    sanitize question.tag_list
+      .map { |tag| link_to "##{tag}", tag_path(tag), class: "tag" }.join(" ")
   end
 
-  def tag_path(t)
-    "/ezanswer/tag/#{t}"
+  def tag_path(tag_id)
+    "/ezanswer/tag/#{tag_id}"
   end
 end
