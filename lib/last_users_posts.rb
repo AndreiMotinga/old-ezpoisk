68ae76863eccbc7d20d5a54b7b5a4eb1d290e2e4
Andrei Motinga <andrew.motinga@gmail.com>
2016-03-15 22:05:41 -0400

    fix bug with active

--- lib/last_users_posts.rb
+++ lib/last_users_posts.rb
@@ -1,8 +1,8 @@
 class LastUsersPosts
   def last_posts(num)
-    posts = RePrivate.last(100)
+    posts = RePrivate.active.last(100)
     [ReAgency, ReCommercial, ReFinance, Service, JobAgency, Job, Sale].each do |model|
-      posts += model.last(100)
+      posts += model.active.last(100)
     end
     posts.sort_by(&:created_at).last(num).reverse
   end
