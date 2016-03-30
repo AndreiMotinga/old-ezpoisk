c8cf5a78adf7604d9c32ceb4bf575574a65d6321
Andrei Motinga <andrew.motinga@gmail.com>
2016-03-30 16:22:57 -0400

    remove html 5 stuf

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ActiveRecord::Base
     :trackable, :timeoutable, :lockable, :async, :omniauthable,
     :omniauth_providers => [:facebook, :google_oauth2, :vkontakte]
 
-  # after_create :geolocate_user, :send_emails
   after_create :send_emails
 
   acts_as_voter
@@ -51,10 +50,6 @@ class User < ActiveRecord::Base
   end
 
   private
-
-  def geolocate_user
-    GeolocateUserJob.perform_async(id)
-  end
 
   def send_emails
     AdminMailerJob.perform_async(id, "User")
