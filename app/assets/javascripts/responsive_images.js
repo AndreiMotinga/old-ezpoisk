932c749dd4b96e70c84cf7c1594ca7d7b08571b7
Andrei Motinga <andrew.motinga@gmail.com>
2016-04-08 16:19:47 -0400

    improves questions mobile views

--- app/assets/javascripts/responsive_images.js
+++ app/assets/javascripts/responsive_images.js
@@ -1,3 +1,5 @@
 $(document).ready(function() {
   $("img").addClass("img-responsive");
+  $(".answer iframe").wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
+  $(".answer iframe").addClass("embed-responsive-item")
 });
