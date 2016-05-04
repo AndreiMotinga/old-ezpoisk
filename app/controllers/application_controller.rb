6106c3d9144cf9d8b975f6adba37ebd47dfd72a6
Andrei Motinga <andrew.motinga@gmail.com>
2016-05-04 09:43:41 -0400

    don't set user location for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :set_location
+  # before_action :set_location
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :check_rack_mini_profiler
   before_action :redirect_to_https
