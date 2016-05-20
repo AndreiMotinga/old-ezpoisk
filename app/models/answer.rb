601dbd6fb959093898ca72805fbfe0d084ef206c
Andrei Motinga <andrew.motinga@gmail.com>
2016-05-20 17:23:25 -0400

    adds specs for answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -8,11 +8,11 @@ class Answer < ActiveRecord::Base
   scope :by_score, -> { all.sort_by(&:score).reverse }
 
   def self.this_week
-    where("created_at > ?", Date.today.at_beginning_of_week).count
+    where("created_at > ?", Date.current.at_beginning_of_week).count
   end
 
   def score
-    get_upvotes.size - get_downvotes.size
+    get_upvotes.count - get_downvotes.count
   end
 
   def user_avatar
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -3,4 +3,31 @@ require "rails_helper"
 describe Answer do
   it { should belong_to(:user) }
   it { should belong_to(:question) }
+
+  describe ".this_week" do
+    it "returns amount of answers created during current week" do
+      create(:answer, created_at: Date.current)
+      create(:answer, created_at: 1.week.ago)
+
+      result = Answer.this_week
+
+      expect(result).to eq 1
+    end
+  end
+
+  describe ".score" do
+    it "returns upvotes minues downvotes" do
+      answer = build(:answer)
+      allow(answer)
+        .to receive_message_chain(:get_upvotes, :count)
+        .and_return(4)
+      allow(answer)
+        .to receive_message_chain(:get_downvotes, :count)
+        .and_return(1)
+
+      result = answer.score
+
+      expect(result).to eq 3
+    end
+  end
 end
