0a970b6302342cda487e8160c612012a04d6fa16
Andrei Motinga <andrew.motinga@gmail.com>
2016-05-21 18:31:50 -0400

    adds voting specs

--- spec/factories/answer.rb
+++ spec/factories/answer.rb
@@ -1,5 +1,13 @@
 FactoryGirl.define do
   factory :answer do
     text { Faker::Lorem.paragraph(20) }
+
+    trait :with_question do
+      question
+    end
+
+    trait :with_user do
+      user
+    end
   end
 end
---
+++ spec/features/answers/user_votes_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+feature "Voting on answers" do
+  context "user logged in" do
+    scenario "user upvotes answer", js: true do
+      user = create_and_login_user
+      answer = create(:answer, :with_question, user: user)
+
+      visit question_path(answer.question)
+      click_on "Upvote"
+      wait_for_ajax
+      answer.reload
+
+      expect(page).to have_content("Voted | 1")
+      expect(answer.score).to eq 1
+    end
+
+    scenario "user downvotes answer", js: true do
+      user = create_and_login_user
+      answer = create(:answer, :with_question, user: user)
+
+      visit question_path(answer.question)
+      click_on "Downvote"
+      wait_for_ajax
+      answer.reload
+
+      expect(page).to have_content("Voted | -1")
+      expect(answer.score).to eq(-1)
+    end
+  end
+end
