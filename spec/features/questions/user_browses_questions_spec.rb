a8e6f4c2969c9b795d0757f723870405903ff97c
Andrei Motinga <andrew.motinga@gmail.com>
2016-05-24 20:50:02 -0400

    fix smoke tests for question

--- spec/features/questions/user_browses_questions_spec.rb
+++ spec/features/questions/user_browses_questions_spec.rb
@@ -1,26 +1,28 @@
 require "rails_helper"
 
-# todo replace with viwes specs
-# feature "User browses questions" do
-#   scenario "success" do
-#     question = create :question
-#     create :answer, question: question
-#
-#     visit questions_path
-#
-#     expect(page).to have_content question.title
-#   end
-# end
-#
-# feature "User browses question" do
-#   scenario "success when user logged in" do
-#     create_and_login_user
-#     q = create :question
-#     a = create :answer, question: q
-#
-#     visit question_path(q)
-#
-#     expect(page).to have_content q.title
-#     expect(page).to have_content a.text
-#   end
-# end
+feature "User browses questions" do
+  scenario "success" do
+    user = create(:user)
+    profile = create(:profile, user: user)
+    question = create :question
+    create :answer, question: question, user: user
+
+    visit questions_path
+
+    expect(page).to have_content question.title
+  end
+end
+
+feature "User browses question" do
+  scenario "success when user logged in" do
+    create_and_login_user
+    q = create :question
+    user = create(:user)
+    a = create :answer, question: q, user: user
+
+    visit question_path(q)
+
+    expect(page).to have_content q.title
+    expect(page).to have_content a.text
+  end
+end
