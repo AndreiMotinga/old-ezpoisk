de7b048c61dcf541f8225b11fbb513dcd9a90ebc
Andrei Motinga <andrew.motinga@gmail.com>
2016-05-27 15:49:15 -0400

    improves news_importer

--- lib/news_importer.rb
+++ lib/news_importer.rb
@@ -4,21 +4,42 @@ class NewsImporter
   USER_ID = 181 # ez
 
   def self.import
+    @records = []
     NEWS_CATEGORIES.each do |link|
       xml = Nokogiri::XML(open(link))
-      create_post(xml)
+      parse_posts(xml)
+    end
+    create_posts
+  end
+
+  private_class_method
+
+  def self.parse_posts(xml)
+    xml.xpath("//item").each do |item|
+      @records << [formatted_record(item)]
     end
   end
 
-  def self.create_post(xml)
-    xml.xpath("//item").each do |item|
-      Post.create(user_id: @user_id,
-                  title: item.at("title").text,
-                  link: convert_link(item.at("link").text),
-                  text: item.at("description").text,
-                  visible: false,
-                  created_at: item.at("pubDate").text)
-    end
+  def self.formatted_record(item)
+    <<-EOL
+      ('#{USER_ID}',
+      '#{item.at('title').text}',
+      '#{convert_link(item.at('link').text)}',
+      '#{item.at('description').text}',
+      '#{false}',
+      '#{item.at('pubDate').text}',
+      '#{Time.current}')
+    EOL
+  end
+
+  def self.create_posts
+    sql = <<-EOL
+      INSERT INTO posts (
+        user_id, title, link, text, visible, created_at,updated_at
+      )
+      VALUES #{@records.join(', ')}
+    EOL
+    ActiveRecord::Base.connection.execute(sql)
   end
 
   def self.convert_link(url)
---
+++ spec/lib/news_importer_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+describe "NewsImporter" do
+  it "imports news" do
+    user = create(:user, id: 181)
+    stub_xml
+
+    NewsImporter.import
+    result = Post.invisible.count
+
+    expect(result).to_not eq 0
+  end
+
+  def stub_xml
+    NEWS_CATEGORIES.each do |link|
+      stub_request(:get, link).to_return(status: 200, :body => file)
+    end
+  end
+
+  def file
+    File.new("#{Rails.root}/spec/support/fixtures/posts/yandex_auto.xml").read
+  end
+end
