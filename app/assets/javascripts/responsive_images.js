96dfb33c87828adcf97bbc3d331bcdb7566adb4e
Andrei Motinga <andrew.motinga@gmail.com>
2016-07-14 19:30:11 -0400

    fix service image

--- app/assets/javascripts/responsive_images.js
+++ app/assets/javascripts/responsive_images.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  $(".description img").addClass("img-responsive");
   $(".answer img").addClass("img-responsive");
   $(".answer iframe").wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
   $(".answer iframe").addClass("embed-responsive-item");
