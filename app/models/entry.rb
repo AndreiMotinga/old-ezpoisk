9ead72d01bb175b20d8d83e54170618ca3efe102
Andrei Motinga <andrew.motinga@gmail.com>
2016-07-15 17:57:40 -0400

    update todays entry count

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,6 +1,6 @@
 class Entry < ActiveRecord::Base
   belongs_to :enterable, polymorphic: true
 
-  scope :today, -> { where("created_at > ?", Time.zone.yesterday) }
+  scope :today, -> { where("created_at > ?", Time.zone.today) }
   scope :week, -> { where("created_at > ?", Date.current.at_beginning_of_week) }
 end
