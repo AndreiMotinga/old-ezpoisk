55b1464aad0683804716b3d16f8483f434a215e9
Andrei Motinga <andrew.motinga@gmail.com>
2016-07-18 19:47:09 -0400

    improves seo

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,6 +1,6 @@
 class Entry < ActiveRecord::Base
   belongs_to :enterable, polymorphic: true
 
-  scope :today, -> { where("created_at > ?", Time.zone.today) }
-  scope :week, -> { where("created_at > ?", Date.current.at_beginning_of_week) }
+  scope :today, -> { where("created_at > ?", Date.today) }
+  scope :week, -> { where("created_at > ?", Date.today.at_beginning_of_week) }
 end
