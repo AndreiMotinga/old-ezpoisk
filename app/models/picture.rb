c3f4d10e10182a08f5728fc8e7f28e23a78045a3
Andrei Motinga <andrew.motinga@gmail.com>
2016-07-18 11:52:05 -0400

    update picture medium size

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -4,7 +4,7 @@ class Picture < ActiveRecord::Base
   delegate :logo, to: :imageable, prefix: true
 
   has_attached_file(:image,
-                    styles: { medium: ["300x170#", :jpg], large: ["x450", :jpg] },
+                    styles: { medium: ["x120", :jpg], large: ["x450", :jpg] },
                     default_url: "https://s3.amazonaws.com/ezpoisk/missing.png")
   validates_attachment_content_type :image, content_type: %r{\Aimage\/.*\Z}
   validates_attachment_file_name :image, matches: [/png\Z/i, /jpe?g\Z/i]
