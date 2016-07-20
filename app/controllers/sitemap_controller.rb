82b6c8fc3a44d759e732bb35a4bb74d3424eca6a
Andrei Motinga <andrew.motinga@gmail.com>
2016-07-19 22:23:42 -0400

    update sitemap

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -1,6 +1,7 @@
 class SitemapController < ApplicationController
   def show
-    sitemap = "sitemap" + params[:id].to_i.to_s
+    sitemap = "sitemap"
+    sitemap += params[:id].to_i.to_s if params[:id]
     url = "https://s3.amazonaws.com/ezpoisk/sitemaps/#{sitemap}.xml.gz"
     data = open(url)
     send_data data.read, type: data.content_type
