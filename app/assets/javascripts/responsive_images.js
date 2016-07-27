f07ef365782edc11c83bc0cd5f63585411800b5e
Andrei Motinga <andrew.motinga@gmail.com>
2016-07-27 11:42:02 -0400

    add responsive image to news posts

--- app/assets/javascripts/responsive_images.js
+++ app/assets/javascripts/responsive_images.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $(".description img").addClass("img-responsive");
+  $(".post img").addClass("img-responsive");
   $(".answer img").addClass("img-responsive");
   $(".answer iframe").wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
   $(".answer iframe").addClass("embed-responsive-item");
