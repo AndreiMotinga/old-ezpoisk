634de040ba788a16dca1687cf5414dad3b107909
Andrei Motinga <andrew.motinga@gmail.com>
2016-08-08 17:34:20 -0400

    don't init summer note if it's already there

--- app/assets/javascripts/summernote_init.js
+++ app/assets/javascripts/summernote_init.js
@@ -1,46 +1,47 @@
-document.addEventListener("turbolinks:load", function() {
-  $(".summernote-lg").summernote({ height: 400 });
+$(document).ready(init);
+document.addEventListener("turbolinks:load", init)
 
-  $(".news-summernote").summernote({
-    height: 400
-  });
+function init(){
+  // look for div initialized by summernote
+  var initialized = $(".summernote + .note-editor").length;
+  if(!initialized) {
+    initSummernote();
+  }
+}
 
-  $(function() {
-    var type = $("#pictures-data").data("type");
-    var id = $("#pictures-data").data("id");
-
-    return $(".summernote").summernote({
-      lang: 'ru',
-      height: 80,
-      codemirror: {
-        lineNumbers: true,
-        tabSize: 2,
-        theme: "solarized light"
-      },
-      callbacks: {
-        onImageUpload: function(files) {
-          return sendFile(files[0], type, id);
-        }
+function initSummernote(){
+  $(".summernote").summernote({
+    lang: 'ru',
+    height: 200,
+    codemirror: {
+      lineNumbers: true,
+      tabSize: 2,
+      theme: "solarized light"
+    },
+    callbacks: {
+      onImageUpload: function(files) {
+        return sendFile(files[0]);
       }
-    });
-
-    function sendFile(file, type, id) {
-      var data = new FormData();
-      data.append('picture[image]', file);
-      data.append('picture[imageable_id]', id);
-      data.append('picture[imageable_type]', type);
-      return $.ajax({
-        data: data,
-        type: 'POST',
-        url: '/dashboard/summernote',
-        cache: false,
-        contentType: false,
-        processData: false,
-        success: function(data) {
-          return $(".summernote").summernote("insertImage", data.url);
-        }
-      });
     }
-
   });
-});
+}
+
+function sendFile(file) {
+  var type = $("#pictures-data").data("type"),
+      id = $("#pictures-data").data("id"),
+      data = new FormData();
+  data.append('picture[image]', file);
+  data.append('picture[imageable_id]', id);
+  data.append('picture[imageable_type]', type);
+  return $.ajax({
+    data: data,
+    type: 'POST',
+    url: '/dashboard/summernote',
+    cache: false,
+    contentType: false,
+    processData: false,
+    success: function(data) {
+      return $(".summernote").summernote("insertImage", data.url);
+    }
+  });
+}
