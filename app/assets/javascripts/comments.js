91402d1c8cc9cc2cc954183c62b64dd3de6b07b1
Andrei Motinga <andrew.motinga@gmail.com>
2016-08-08 23:04:19 -0400

    improves comments

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -4,3 +4,18 @@ $(document).on('click','.comment-reply', function(e){
   form.toggle();
   form.find("input").focus();
 });
+
+$(document).on('keypress','[data-action="enter"]', function(e){
+  if(e.which == 13 && e.shiftKey){
+    var count = $(this).attr('rows');
+    count = parseInt(count) + 1;
+    $(this).attr('rows', count);
+  }
+  else if (e.which == 13 && !e.shiftKey){
+    $(this).closest("form").submit();
+    e.preventDefault();
+    $(this).val("");
+    return false;
+   }
+});
+
