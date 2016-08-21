360e485208c0062e4affe7c1c6c811806b982c98
Andrei Motinga <andrew.motinga@gmail.com>
2016-08-20 21:24:54 -0400

    remove default city as new york

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,10 +1,10 @@
 module FormHelper
   def state_id
-    params[:state_id] || current_user.try(:state_id) || 32
+    params[:state_id] || current_user.try(:state_id) || 1
   end
 
   def city_id
-    params[:city_id] || current_user.try(:city_id) || 18_031
+    params[:city_id] || current_user.try(:city_id)
   end
 
   def state_cities
