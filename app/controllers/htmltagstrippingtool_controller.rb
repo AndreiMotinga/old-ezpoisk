6ef32974df759d8e86b0180d49617b15416165e6
Andrei Motinga <andrew.motinga@gmail.com>
2016-08-20 20:32:00 -0400

    remove htmlstrippingtool

--- app/controllers/htmltagstrippingtool_controller.rb
+++
@@ -1,6 +0,0 @@
-class HtmltagstrippingtoolController < ApplicationController
-  layout "home"
-
-  def index
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -87,7 +87,6 @@ Rails.application.routes.draw do
     mount RailsAdmin::Engine => "/teacup", as: "rails_admin"
   end
 
-  get "htmltagstrippingtool", to: "htmltagstrippingtool#index"
   get "about", to: "home#about"
   root to: "home#index"
 end
