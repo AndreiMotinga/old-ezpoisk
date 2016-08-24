dc1b4da30f1bfbe00f6798ed487b722b7311d2bf
Andrei Motinga <andrew.motinga@gmail.com>
2016-08-23 21:22:50 -0400

    adds contacts page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -16,4 +16,7 @@ class HomeController < ApplicationController
 
   def about
   end
+
+  def contacts
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -90,5 +90,6 @@ Rails.application.routes.draw do
   end
 
   get "about", to: "home#about"
+  get "contacts", to: "home#contacts"
   root to: "home#index"
 end
