6a1aed86d3ea01cebe7faad43444e67d57ca6a94
Andrei Motinga <andrew.motinga@gmail.com>
2016-08-24 18:40:43 -0400

    sort by priority then updated at

--- app/models/concerns/filterable.rb
+++ app/models/concerns/filterable.rb
@@ -45,7 +45,7 @@ module Filterable
 
   module ClassMethods
     def filter(params)
-      results = params[:sorted] ? active : active.order("priority desc")
+      results = params[:sorted] ? active : active.order("priority desc, updated_at desc")
       params.each do |key, value|
         results = results.public_send(key, value) unless value.blank?
       end
