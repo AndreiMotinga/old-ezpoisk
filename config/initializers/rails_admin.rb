b0b6439f1faed4b3f3038093a45c99d5c9e78a76
Andrei Motinga <andrew.motinga@gmail.com>
2016-08-30 11:34:25 -0400

    remove summer note from listings in rails admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -34,16 +34,6 @@ RailsAdmin.config do |config|
     configure :tag_list  do
       partial 'tag_list_with_autocomplete'
     end
-    edit do
-      field :text do
-        render do
-          bindings[:view].render(
-            partial: "text",
-            locals: { key: :text, field: self, form: bindings[:form] }
-          )
-        end
-      end
-    end
   end
 
   config.model "Answer" do
@@ -54,34 +44,6 @@ RailsAdmin.config do |config|
           bindings[:view].render(
             partial: "text",
             locals: { key: :text, field: self, form: bindings[:form] }
-          )
-        end
-      end
-    end
-  end
-
-  config.model "Job" do
-    include_all_fields
-    edit do
-      field :description do
-        render do
-          bindings[:view].render(
-            partial: "text",
-            locals: { key: :description, field: self, form: bindings[:form] }
-          )
-        end
-      end
-    end
-  end
-
-  config.model "RePrivate" do
-    include_all_fields
-    edit do
-      field :description do
-        render do
-          bindings[:view].render(
-            partial: "text",
-            locals: { key: :description, field: self, form: bindings[:form] }
           )
         end
       end
