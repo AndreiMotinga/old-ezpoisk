12df766ca175456db1369ff8a34971cc352ec35f
Andrei Motinga <andrew.motinga@gmail.com>
2016-09-20 15:16:30 -0400

    mix services

---
+++ app/jobs/service_mixer_job.rb
@@ -0,0 +1,12 @@
+class ServiceMixerJob
+  include Sidekiq::Worker
+
+  def perform
+    Service.order("RANDOM()")
+           .each {|s| s.update_column(:updated_at, Time.zone.now) }
+  end
+end
+
+Sidekiq::Cron::Job.create(name: "every 3 hours",
+                          cron: "0 */3 * * *",
+                          class: "DataAggregatorJob")
