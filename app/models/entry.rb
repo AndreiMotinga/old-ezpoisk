0fe427923d4d71422cfe37bc618ec25e4decf18a
Andrei Motinga <andrew.motinga@gmail.com>
2016-09-26 15:54:26 -0400

    show question on home page

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -7,6 +7,6 @@ class Entry < ActiveRecord::Base
 
   scope :desc, -> { order("created_at desc") }
   scope :articles, -> { where(enterable_type: %w(Answer Post)) }
-  scope :news, -> { where(enterable_type: %w(Answer Post Review)) }
+  scope :news, -> { where(enterable_type: %w(Answer Post Review Question)) }
   scope :listings, -> { where(enterable_type: %w(RePrivate Sale Service Job)) }
 end
