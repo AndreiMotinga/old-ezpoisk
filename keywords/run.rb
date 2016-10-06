0399990cfd32a571a9ddcad64a5fa9be87b93bca
Andrei Motinga <andrew.motinga@gmail.com>
2016-10-05 20:03:51 -0400

    removes keywords text files

--- keywords/run.rb
+++
@@ -1,9 +0,0 @@
-File.open('keywords/keywords.txt', 'w+') do |f|
-  keys.each do |key|
-    cities.each do |city|
-      f.puts "#{key} #{city}"
-      f.puts "'#{key} #{city}'"
-      f.puts "[#{key} #{city}]"
-    end
-  end
-end
