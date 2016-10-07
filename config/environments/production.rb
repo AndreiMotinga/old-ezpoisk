c3f0118c3e83fb8453e69dba250ed2072f6347e0
Andrei Motinga <andrew.motinga@gmail.com>
2016-10-07 03:20:07 -0400

    enforce ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,8 +34,7 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # todo-production
-  # config.action_controller.asset_host = "//drex16ydhdd8s.cloudfront.net"
+  config.action_controller.asset_host = "//drex16ydhdd8s.cloudfront.net"
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
@@ -47,8 +46,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # todo-production
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
