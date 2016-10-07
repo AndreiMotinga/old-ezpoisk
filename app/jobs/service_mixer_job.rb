511bec50b4ee255c02a4bcb6bd79045acf4fea57
Andrei Motinga <andrew.motinga@gmail.com>
2016-10-07 11:20:08 -0400

    disable service mixer for now to not mess with order

--- app/jobs/service_mixer_job.rb
+++ app/jobs/service_mixer_job.rb
@@ -2,10 +2,11 @@ class ServiceMixerJob
   include Sidekiq::Worker
 
   def perform
-    Service.random.each {|s| s.update_attribute(:created_at, Time.zone.now) }
+    # todo fix it
+    # Service.random.each {|s| s.update_attribute(:created_at, Time.zone.now) }
   end
 end
 
-Sidekiq::Cron::Job.create(name: "every 3 hours",
-                          cron: "0 */3 * * *",
-                          class: "ServiceMixerJob")
+# Sidekiq::Cron::Job.create(name: "every 3 hours",
+#                           cron: "0 */3 * * *",
+#                           class: "ServiceMixerJob")
