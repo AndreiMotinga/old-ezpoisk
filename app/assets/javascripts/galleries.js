11f88ae41af64f4a1ab565b8441fd4aa830066f3
Andrei Motinga <andrew.motinga@gmail.com>
2016-10-08 20:47:42 -0400

    fix gallery blowing up

--- app/assets/javascripts/galleries.js
+++ app/assets/javascripts/galleries.js
@@ -1,10 +1,19 @@
 document.addEventListener("turbolinks:load", function() {
-  $('.pgwSlideshow').pgwSlideshow({
-    transitionEffect : 'fading',
-    intervalDuration: 5000,
-    autoSlide: true
-  });
+  initSlideShow();
+
   $('.show .ps-current a').touchTouch();
 
   $('[data-action="touchTouch"]').touchTouch();
 });
+
+function initSlideShow(){
+  if (!$('.pgwSlideshow').length) { return; }
+  var initialized = $(".pgwSlideshow .ps-current").length;
+  if (!initialized){
+    $('.pgwSlideshow').pgwSlideshow({
+      transitionEffect : 'fading',
+      intervalDuration: 5000,
+      autoSlide: true
+    });
+  }
+}
