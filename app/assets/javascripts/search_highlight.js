a2ca1b3f436608a1a16125d65b803bcf7e529d0b
Andrei Motinga <andrew.motinga@gmail.com>
2016-10-13 19:07:00 -0400

    fix search highlight

--- app/assets/javascripts/search_highlight.js
+++ app/assets/javascripts/search_highlight.js
@@ -4,9 +4,9 @@ $(document).ajaxComplete(highlight);
 function highlight(){
   var el = $("[data-id='listings']");
   var term = el.data("term");
+  var html = el.html();
 
-  if(!!term) {
-    var html = el.html();
+  if(!!term && !!html) {
     $.each(term.split(" "), function(i, word){
       var regex = new RegExp(word, "gi");
       html = html.replace(regex, "<mark>"+word+"</mark>");
