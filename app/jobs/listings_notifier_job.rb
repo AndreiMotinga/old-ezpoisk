b063b5d8a2da2170e749fc42e8bb6b7e5a1b9588
Andrei Motinga <andrew.motinga@gmail.com>
2016-10-16 11:10:21 -0400

    fix listing notifier job

--- app/jobs/listings_notifier_job.rb
+++ app/jobs/listings_notifier_job.rb
@@ -5,7 +5,7 @@ class ListingsNotifierJob
   def perform(id, model)
     return if Rails.env.development?
     record = model.constantize.find_by_id(id)
-    return unless record.contact_email
+    return unless record.try(:contact_email)
     return if record.user.try(:id) == 1 # ez
     ListingsMailer.ten_visits(record).deliver_now
   end
