c87dcc5e5978ad7e0d011e930bc6b59dcf7209ec
Andrei Motinga <andrew.motinga@gmail.com>
2016-10-18 19:57:35 -0400

    remove time job

--- app/jobs/partner_impressions_job.rb
+++ app/jobs/partner_impressions_job.rb
@@ -3,8 +3,6 @@ class PartnerImpressionsJob
   include Sidekiq::Worker
 
   def perform(ids)
-    # todo rewrite using increment_counter
-    # add test
     partners = Partner.where(id: ids)
     partners.each do |partner|
       partner.increment!(:impressions_count)
--- app/jobs/time_job.rb
+++
@@ -1,9 +0,0 @@
-# todo remove
-class TimeJob
-  include Sidekiq::Worker
-  sidekiq_options queue: 'critical'
-
-  def perform
-    puts("tick")
-  end
-end
