8734a8debccfab97a06ad8445babcf3703583184
Andrei Motinga <andrew.motinga@gmail.com>
2016-10-20 16:34:01 -0400

    return empty cities if state wasn't selected

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,14 +1,18 @@
 class CitiesController < ApplicationController
   def index
     slug = params[:state_slug]
-    if slug
+    id = params[:state_id]
+    if slug.present?
       state = State.find_by_slug(slug)
       @cities = City.where(state_id: state.id).or(City.where(slug: City::ALL))
       @id = "slug"
-    else
+    elsif id.present?
       state = State.find(params[:state_id])
       @cities = City.where(state_id: state.id).or(City.where(slug: City::ALL))
       @id = "id"
+    else
+      @cities = []
+      @id = ""
     end
   end
 end
