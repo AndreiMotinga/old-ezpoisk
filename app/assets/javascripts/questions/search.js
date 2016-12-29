3a42c67fb4d8b640a7c48a8e128f16ff4df6de19
Andrei Motinga <andrei@Andreis-MacBook-Air.local>
2016-12-28 21:12:00 -0500

    bug fix - Question autocomplete doesn't work in production.
    .gitignore included questions, which apparently led to app/assets/javascripts/questions not being tracked, as a result id wouldn't compile on heroku.

---
+++ app/assets/javascripts/questions/search.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+  $("#q-search").autocomplete({
+    source: "/search_suggestions",
+
+    // open link in new tab
+    select: function( event, ui ) {
+      event.preventDefault();
+      openNewTab(ui.item.value);
+    },
+
+    // replace input with user selected li
+    focus: function(event, ui) {
+      event.preventDefault();
+      // $(".q-search").val(ui.item.label);
+    },
+
+    // show menu users clicks on already populated search field
+  }).focus(function(){
+    $(this).autocomplete('search', $(this).val())
+  });
+
+  function openNewTab(uri){
+    $('#linkToOpen').remove();
+    var link = document.createElement('a');
+    // link.target = '_blank';
+    link.href = uri;
+    link.id = 'linkToOpen';
+
+    document.body.appendChild(link);
+    $('#linkToOpen')[0].click();
+    $('#linkToOpen')[0].remove();
+  }
+
+  // monkey patch el to highlight search term
+  $.ui.autocomplete.prototype._renderItem = function (ul, item) {
+    var t = String(item.label).replace(
+        new RegExp(this.term, "gi"),
+        "<span class='ui-state-highlight'>$&</span>");
+    return $("<li></li>")
+      .data("item.autocomplete", item)
+      .append( t )
+      .appendTo(ul);
+  };
+});
