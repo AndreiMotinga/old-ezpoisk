9e38c53d5fcada4f1f4161b04c000ab3d1956e51
Andrei Motinga <andrew.motinga@gmail.com>
2017-07-18 14:03:03 -0400

    allow to add/edit posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,8 +17,7 @@ class PostsController < ApplicationController
 
     if @post.save
       SlackNotifierJob.perform_async(@post.id, "Post")
-      # redirect_to @post, notice: I18n.t(:p_created)
-      redirect_to @post, notice: "Пост добавлен"
+      redirect_to @post, notice: I18n.t(:p_created)
     else
       render :new
     end
@@ -26,6 +25,24 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = current_user.posts.find(params[:id])
+  end
+
+  def update
+    @post = current_user.posts.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post), notice: I18n.t(:p_updated)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice: I18n.t(:p_destroyed)
   end
 
   private
