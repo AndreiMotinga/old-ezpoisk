197bb7bd20a468529459a154dd469de884beeb96
Andrei Motinga <andrew.motinga@gmail.com>
2017-07-19 11:56:38 -0400

    fix filtering

--- spec/features/listings/user_filters_listings_spec.rb
+++ spec/features/listings/user_filters_listings_spec.rb
@@ -1,14 +1,31 @@
 require "rails_helper"
 
 feature "User filters listings" do
-  scenario "success" do
+  scenario "by state" do
     listing = create :listing
 
     visit listings_path(kind: listing.kind)
-
     select(listing.state.name, from: :state)
-    select I18n.t(listing.category), from: :category
-    select I18n.t(listing.subcategory), from: :subcategory
+    click_on "Обновить"
+
+    expect(page).to have_content listing.title
+  end
+
+  scenario "by kind" do
+    listing = create :listing
+
+    visit listings_path(kind: listing.kind)
+    select I18n.t(listing.kind), from: :kind
+    click_on "Обновить"
+
+    expect(page).to have_content listing.title
+  end
+
+  scenario "by kind" do
+    listing = create :listing
+
+    visit listings_path(kind: listing.kind)
+    select I18n.t(listing.kind), from: :kind
     click_on "Обновить"
 
     expect(page).to have_content listing.title
--- spec/requests/listings/index_spec.rb
+++ spec/requests/listings/index_spec.rb
@@ -2,92 +2,10 @@ require "rails_helper"
 
 describe "Listings" do
   describe "GET #index" do
-    context "Real Estate" do
-      context "listings/real-estate" do
-        it "renders index temlpate" do
-          get "/listings/real-estate"
+    it "renders index temlpate" do
+      get "/listings"
 
-          expect(response).to render_template(:index)
-        end
-      end
-
-      context "category=leasing&kind=real-estate" do
-        it "renders index temlpate" do
-          get "/listings?category=leasing&kind=real-estate"
-
-          expect(response).to render_template(:index)
-        end
-      end
-
-      context "category=renting&kind=real-estate" do
-        it "renders index temlpate" do
-          get "/listings?category=renting&kind=real-estate"
-
-          expect(response).to render_template(:index)
-        end
-      end
-    end
-
-    context "Jobs" do
-      context "listings/jobs" do
-        it "renders index temlpate" do
-          get "/listings/jobs"
-
-          expect(response).to render_template(:index)
-        end
-      end
-
-      context "listings?category=wanted&kind=jobs" do
-        it "renders index temlpate" do
-          get "/listings?category=wanted&kind=jobs"
-
-          expect(response).to render_template(:index)
-        end
-      end
-
-      context "listings?category=seeking&kind=jobs" do
-        it "renders index temlpate" do
-          get "/listings?category=seeking&kind=jobs"
-
-          expect(response).to render_template(:index)
-        end
-      end
-    end
-
-    context "Sales" do
-      context "/listings/sales" do
-        it "renders index temlpate" do
-          get "/listings/sales"
-
-          expect(response).to render_template(:index)
-        end
-      end
-
-      context "/listings?category=buying&kind=sales" do
-        it "renders index temlpate" do
-          get "/listings?category=buying&kind=sales"
-
-          expect(response).to render_template(:index)
-        end
-      end
-
-      context "/listings?category=giving&kind=sales" do
-        it "renders index temlpate" do
-          get "/listings?category=giving&kind=sales"
-
-          expect(response).to render_template(:index)
-        end
-      end
-    end
-
-    context "Services" do
-      context "/listings/services" do
-        it "renders index temlpate" do
-          get "/listings/services"
-
-          expect(response).to render_template(:index)
-        end
-      end
+      expect(response).to render_template(:index)
     end
   end
 
