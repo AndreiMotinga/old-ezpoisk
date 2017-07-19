2b24ba272411c2eadd9eadb2a9d11ade53d14422
Andrei Motinga <andrew.motinga@gmail.com>
2017-07-18 21:04:23 -0400

    remove users page

--- spec/features/users/user_browses_profile_spec.rb
+++ spec/features/users/user_browses_profile_spec.rb
@@ -5,7 +5,7 @@ feature "User browses profile" do
   scenario "visits page" do
     user = create :user
 
-    visit user_path(user)
+    visit profile_path(user)
     expect(page).to have_content user.name
     expect(page).to have_content user.about
   end
@@ -14,7 +14,7 @@ feature "User browses profile" do
     user = create :user
     listing = create :listing, user: user
 
-    visit user_path(user)
+    visit profile_path(user)
     first(:link, "Объявления").click
 
     expect(page).to have_content listing.title
@@ -25,7 +25,7 @@ feature "User browses profile" do
     q = create :question
     answer = create :answer, user: user, question: q, title: q.title
 
-    visit user_path(user)
+    visit profile_path(user)
     first(:link, "Ответы").click
     expect(page).to have_content answer.title
   end
