c1b2cabd6c35fe8c999dd907af9023410f900e48
Andrei Motinga <andrew.motinga@gmail.com>
2017-07-18 23:01:11 -0400

    reorganize routes file

--- config/routes.rb
+++ config/routes.rb
@@ -5,28 +5,6 @@ Rails.application.routes.draw do
   # TODO: figure out why it's here
   default_url_options host: ENV.fetch("APPLICATION_HOST")
 
-  resources :posts
-  resources :questions, only: [:new, :create, :show] do
-    collection do
-      get "unanswered", to: "questions#unanswered"
-      get "unanswered/:tag", to: "questions#unanswered_tag", as: :unanswered_tag
-    end
-  end
-  resources :answers do
-    collection do
-      get "tag/:tag", to: "answers#tag", as: :tag
-    end
-  end
-
-  resources :search_suggestions, only: [:index]
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  get "sitemaps/sitemap.:format.:compression", to: "sitemap#show"
-  get "sitemaps/sitemap:id.:format.:compression", to: "sitemap#show_id"
-  resources :cities, only: :index
-
-  resources :summernote, only: [:create]
-  resources :users, only: [:edit, :update]
-  resources :pictures, only: [:index, :create, :update, :destroy]
   namespace :listings do
     resources :categories, only: [:index]
     resources :subcategories, only: [:index]
@@ -41,6 +19,20 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :answers do
+    collection do
+      get "tag/:tag", to: "answers#tag", as: :tag
+    end
+  end
+  resources :questions, only: [:new, :create, :show] do
+    collection do
+      get "unanswered", to: "questions#unanswered"
+      get "unanswered/:tag", to: "questions#unanswered_tag", as: :unanswered_tag
+    end
+  end
+
+  resources :posts
+
   resources :profiles, only: :show do
     member do
       get :questions
@@ -49,12 +41,19 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :search_suggestions, only: [:index]
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  get "sitemaps/sitemap.:format.:compression", to: "sitemap#show"
+  get "sitemaps/sitemap:id.:format.:compression", to: "sitemap#show_id"
+  resources :cities, only: :index
+  resources :summernote, only: [:create]
+  resources :users, only: [:edit, :update]
+  resources :pictures, only: [:index, :create, :update, :destroy]
 
   authenticate :user, ->(u) { u.admin? } do
     mount Sidekiq::Web => "/sidekiq"
     mount RailsAdmin::Engine => "/teacup", as: "rails_admin"
   end
 
-  resources :users, only: [:show]
   root to: "home#index"
 end
