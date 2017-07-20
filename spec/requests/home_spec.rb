98c6d71d4760f5e57e919837c07b79c8e74d8b98
Andrei Motinga <andrew.motinga@gmail.com>
2017-07-19 21:30:34 -0400

    fix specs

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -3,10 +3,11 @@ require "rails_helper"
 describe "Home page" do
   describe "GET #index" do
     it "renders the new temlpate" do
+      create_and_login_user
       get "/"
 
       expect(response).to render_template(:index)
-      expect(response.body).to include("ezpoisk.com")
+      expect(response.body).to include("Ответы")
     end
   end
 end
