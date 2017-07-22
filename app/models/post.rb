1d97fdbe3eed1e04f7c2ff4a1daee469105fe84b
Andrei Motinga <andrew.motinga@gmail.com>
2017-07-22 11:01:10 -0400

    make post actionable

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,11 @@ class Post < ApplicationRecord
 
   delegate :avatar, :name_to_show, to: :user
 
+  has_one :action, as: :actionable, dependent: :destroy
+
   after_create :create_action
+
+  def show_url
+    Rails.application.routes.url_helpers.post_url(self)
+  end
 end
