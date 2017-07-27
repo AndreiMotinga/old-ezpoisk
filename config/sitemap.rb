8834723a6bcac49da85c04c2d3d423ca15e3df37
Andrei Motinga <andrew.motinga@gmail.com>
2017-07-27 16:12:50 -0400

    add links to sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -32,26 +32,54 @@ SitemapGenerator::Sitemap.create do
   #     add article_path(article), :lastmod => article.updated_at
   #   end
 
-  add listings_path, priority: 0.8, changefreq: "daily"
+  # add index listings pages
+  %w(работа продажи недвижимость).each do |kind|
+    RU_KINDS[kind]["categories"].each do |cat|
+      RU_KINDS[kind]["subcategories"].each do |sub|
+        if Listing.where(kind: kind, category: cat, subcategory: sub).any?
+          url = search_listings_path(kind: kind, category: cat, subcategory: sub)
+          add url, priority: 0.8, changefreq: "daily"
+        end
+      end
+    end
+  end
+
+  RU_KINDS["услуги"]["categories"].each do |cat|
+    RU_KINDS["услуги"]["subcategories"][cat].each do |sub|
+      if Listing.where(kind: "услуги", category: cat, subcategory: sub).any?
+        url = search_listings_path(kind: "услуги", category: cat, subcategory: sub)
+        add url, priority: 0.8, changefreq: "daily"
+      end
+    end
+  end
+
   Listing.active.find_each do |listing|
     add listing_path(listing),
         priority: 0.4,
         lastmod: listing.updated_at,
-        changefreq: "monthly"
+        changefreq: "weekly"
   end
 
-  add answers_path, priority: 0.8, changefreq: "weekly"
+  add answers_path, priority: 0.8, changefreq: "daily"
   Answer.find_each do |answer|
     add answer_path(answer),
         priority: 0.6,
         lastmod: answer.updated_at,
-        changefreq: "monthly"
+        changefreq: "weekly"
   end
 
-  # User.find_each do |user|
-  #   add user_path(user),
-  #       priority: 0.4,
-  #       lastmod: user.updated_at,
-  #       changefreq: "monthly"
-  # end
+  add questions_path, priority: 0.8, changefreq: "daily"
+  Question.find_each do |answer|
+    add question_path(answer),
+        priority: 0.6,
+        lastmod: answer.updated_at,
+        changefreq: "weekly"
+  end
+
+  User.find_each do |user|
+    add profile_path(user),
+        priority: 0.4,
+        lastmod: user.updated_at,
+        changefreq: "weekly"
+  end
 end
