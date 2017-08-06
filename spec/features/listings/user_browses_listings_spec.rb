f53ebda6779b5413c0006365a51516fdf7b9497a
Andrei Motinga <andrew.motinga@gmail.com>
2017-08-06 16:40:24 -0400

    load css while executing tests

--- spec/features/listings/user_browses_listings_spec.rb
+++ spec/features/listings/user_browses_listings_spec.rb
@@ -2,11 +2,11 @@ require "rails_helper"
 
 feature "User filters listings" do
   scenario "success", js: true do
-    create_list :listing, 20, kind: "недвижимость"
+    create_list :listing, 11, kind: "недвижимость"
 
     visit listings_path(kind: "недвижимость")
     page.execute_script "window.scrollBy(0,10000)"
-    wait_for_ajax
+    # wait_for_ajax
 
     Listing.pluck(:text).map do |text|
       expect(page).to have_content text
