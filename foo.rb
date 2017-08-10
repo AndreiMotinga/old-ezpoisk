c628246a0703181f6909e75ba9f0e0df5f296c19
Andrei Motinga <andrew.motinga@gmail.com>
2017-08-10 13:13:02 -0400

    update categories

--- foo.rb
+++
@@ -1,8 +0,0 @@
-doc = Nokogiri::HTML(p)
-if img = doc.xpath("//img").first
-    p img.attr("src")
-end
-
-if img = Nokogiri::HTML(text).xpath("//img").first
-  update_column(:logo_url, img.attr("src"))
-end
