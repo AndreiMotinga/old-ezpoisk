bcb34c5b46c0c0627d3b57e2a209996b52c0ea9d
Andrei Motinga <andrew.motinga@gmail.com>
2017-08-11 04:12:20 -0400

    fix frozen strings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-#frozen_string_literal: true
+# frozen_string_literal: true
 
 Rails.application.configure do
   config.cache_classes = false
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-#frozen_string_literal: true
+# frozen_string_literal: true
 
 require Rails.root.join("config/smtp")
 Rails.application.configure do
