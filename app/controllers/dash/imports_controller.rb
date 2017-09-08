3cd4c796b93816013dc5c89a4772c2bd247342e7
Andrei Motinga <andrew.motinga@gmail.com>
2017-09-07 22:44:31 -0400

    show 50 at a time

--- app/controllers/dash/imports_controller.rb
+++ app/controllers/dash/imports_controller.rb
@@ -2,6 +2,6 @@ class Dash::ImportsController < ApplicationController
   def index
     @listings = Listing.inactive.includes(:state, :city)
     @listings = @listings.where(kind: params[:kind]) if params[:kind]
-    @listings = @listings.page(params[:page])
+    @listings = @listings.page(params[:page]).per(50)
   end
 end
