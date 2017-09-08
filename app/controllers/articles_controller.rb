0e8bc5bc3c690aaad7ac9c35c6e6bcbebf9af1f3
Andrei Motinga <andrew.motinga@gmail.com>
2017-09-07 21:33:51 -0400

    improve articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,9 @@
 class ArticlesController < ApplicationController
   def index
     @articles = model.includes(:user)
-    @articles = @articles.where(user_id: user_id) if user_id.present?
-    @articles = @articles.where("created_at >= ?", start_date) if params[:start_date].present?
-    @articles = @articles.where("created_at <= ?", end_date) if params[:end_date].present?
+    @articles = @articles.where(user_id: params[:user_id]) if params[:user_id].present?
+    @articles = @articles.where("created_at >= ?", params[:start]) if params[:start].present?
+    @articles = @articles.where("created_at <= ?", params[:end]) if params[:end].present?
     @articles = @articles.order(created_at: :desc).page(params[:page]).per(300)
     respond_to do |format|
       format.html
@@ -15,19 +15,5 @@ class ArticlesController < ApplicationController
 
   def model
     params[:model].present? ? params[:model].constantize : Post
-  end
-
-  def user_id
-    params[:user_id]
-  end
-
-  def start_date
-    start = params[:start_date]
-    DateTime.new(start["year"].to_i, start["month"].to_i, start["day"].to_i)
-  end
-
-  def end_date
-    end_prm = params[:end_date]
-    DateTime.new(end_prm["year"].to_i, end_prm["month"].to_i, end_prm["day"].to_i)
   end
 end
