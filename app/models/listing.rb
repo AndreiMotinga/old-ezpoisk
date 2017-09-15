0dfed58ad12c59f97bc805ff87d3f1b1890cf0b4
Andrei Motinga <andrew.motinga@gmail.com>
2017-09-15 09:56:54 -0400

    update street

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -103,9 +103,11 @@ class Listing < ApplicationRecord
   # TODO: remove
   def self.update_services
     where(kind: "услуги", active: false).find_each do |l|
+      next if l.street.present?
+      next unless l.lat.present? && l.lng.present?
       res = Geokit::Geocoders::GoogleGeocoder.reverse_geocode [l.lat, l.lng].join(",")
       state_id = State.find_by_abbr(res.state)&.id || State.create(abbr: res.state).id
-      city_id = City.find_by_name(res.city)&.id || City.create(state_id: state_id, name: res.city, slug: res.city.parameterize)
+      city_id = City.find_by_name(res.city)&.id || City.create(state_id: state_id, name: res.city, slug: res.city.parameterize).id
 
       l.state_id = state_id || create_state
       l.city_id = city_id if city_id
