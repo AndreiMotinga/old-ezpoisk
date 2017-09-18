fcc5a4e4b1125579a70e5d26ebba69b378d4f7f0
Andrei Motinga <andrew.motinga@gmail.com>
2017-09-18 13:08:54 -0400

    import order by done first

--- app/controllers/dash/imports_controller.rb
+++ app/controllers/dash/imports_controller.rb
@@ -2,6 +2,7 @@ class Dash::ImportsController < ApplicationController
   def index
     @listings = Listing.inactive.includes(:state, :city)
     @listings = @listings.where(kind: params[:kind]) if params[:kind]
-    @listings = @listings.page(params[:page]).per(50)
+    order = "case when done is null then -1 else 1 end desc"
+    @listings = @listings.order(order).page(params[:page]).per(50)
   end
 end
