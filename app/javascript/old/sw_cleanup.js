25bc510545281c5ef1273c312bc1276c22ac9793
Andrei Motinga <andrew.motinga@gmail.com>
2017-09-19 18:30:15 -0400

    add sw_cleanup

---
+++ app/javascript/old/sw_cleanup.js
@@ -0,0 +1,27 @@
+// added to clean up service-worker in case of stale create-react-app
+
+window.__testCleanup = () => {
+  const unregisterSW = () => {
+    return navigator.serviceWorker.getRegistrations()
+    .then((registrations) => {
+      const unregisterPromise = registrations.map((registration) => {
+        return registration.unregister();
+      });
+      return Promise.all(unregisterPromise);
+    });
+  };
+
+  const clearCaches = () => {
+    return window.caches.keys()
+    .then((cacheNames) => {
+      return Promise.all(cacheNames.map((cacheName) => {
+        return window.caches.delete(cacheName);
+      }));
+    });
+  };
+
+  return Promise.all([
+    unregisterSW(),
+    clearCaches(),
+  ]);
+};
