0b59ebb46d8b2ec9d57a1ff2dac3c77452afc653
Andrei Motinga <andrew.motinga@gmail.com>
2017-12-08 19:21:13 -0500

    stop exporting listings

--- app/jobs/listing_exporter_job.rb
+++ app/jobs/listing_exporter_job.rb
@@ -5,10 +5,10 @@ class ListingExporterJob
   include Sidekiq::Worker
 
   def perform(id)
-    return unless Rails.env.production?
-    record = Listing.active.find_by_id(id)
-    return unless record
-    Vk::Exporter.export(record) unless record.vk?
-    Fb::Exporter.export(record) unless record.fb?
+    # return unless Rails.env.production?
+    # record = Listing.active.find_by_id(id)
+    # return unless record
+    # Vk::Exporter.export(record) unless record.vk?
+    # Fb::Exporter.export(record) unless record.fb?
   end
 end
